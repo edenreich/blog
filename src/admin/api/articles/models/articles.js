fee2aafc1abcbc071afd8f362f5fd5c5c2dded91
Eden Reich <eden.reich@gmail.com>
2020-05-10 15:13:00 +0200

    add uuid to each article

--- src/admin/api/articles/models/articles.js
+++ src/admin/api/articles/models/articles.js
@@ -33,9 +33,9 @@ module.exports = {
 
   // Before creating a value.
   // Fired before an `insert` query.
-  // beforeCreate: async (model, _attrs, _options) => {
-  //   model.set('id', uuidv4());
-  // },
+  beforeCreate: async (model, _attrs, _options) => {
+    model.set('uid', uuidv4());
+  },
 
   // After creating a value.
   // Fired after an `insert` query.
