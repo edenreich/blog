54614f4bdd7478a82ff09aa4e83191b331686b83
Eden Reich <eden.reich@gmail.com>
2020-11-20 03:59:12 +0100

    use existing features
    cool, didn't know about draftAndPublish, it's doing all the heavylifting for me :)

--- src/admin/api/articles/models/articles.js
+++ src/admin/api/articles/models/articles.js
@@ -10,16 +10,7 @@ module.exports = {
   lifecycles: {
 
     beforeCreate: async (model) => {
-      model.created_at = new Date;
       model.uuid = uuidv4();
-    },
-
-    beforeUpdate: async (params, model) => {
-      if (model.published === true) {
-        model.published_at = new Date;
-      }
-
-      model.updated_at = new Date;
     },
 
   },
