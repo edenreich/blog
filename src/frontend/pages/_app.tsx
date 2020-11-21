d5b7a8971316aecc283ecd890f903c6e2aa07d33
Eden Reich <eden.reich@gmail.com>
2020-11-21 22:37:23 +0100

    req or headers are optional

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -33,7 +33,7 @@ class Blog extends App<IProps, IState> {
     let pageProps = {};
     let session;
     try {
-      const response: AxiosResponse = await axios.post(`${publicRuntimeConfig.app.url}/api/sessions`, {} ,{ headers:  ctx.req.headers });
+      const response: AxiosResponse = await axios.post(`${publicRuntimeConfig.app.url}/api/sessions`, {} ,{ headers:  ctx?.req?.headers });
       session = response.data;
     } catch (error) {
       session = null;
