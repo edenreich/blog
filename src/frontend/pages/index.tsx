ff7f18e801e4adc68a39a035fa38cf0a6c885bed
Eden Reich <eden.reich@gmail.com>
2020-11-22 04:52:23 +0100

    update blog meta data

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -32,23 +32,29 @@ class IndexPage extends React.Component<IProps> {
 
   render(): JSX.Element {
     const { publicRuntimeConfig } = getConfig();
+    const title = 'Blog | Home Page';
+    const description = 'Welcome to my blog, I\'ll be posting about web app development, native apps, DevOps and more.';
 
     return (
       <div id="home" className="home">
         <Head>
-          <title>Blog | Home Page</title>
+          <title>{title}</title>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="author" content="Eden Reich" />
           <meta name="keywords" content="Eden,Eden Reich,PHP,C++,Typescript,Javascript,CPP,Go,Web" />
-          <meta name="description" content="welcome to my blog, here you may find interesting content about web app development." />
+          <meta name="description" content={description} />
+          <meta property="og:site_name" content="Eden Reich" />
+          <meta property="og:title" content={title} />
+          <meta property="og:image" content="/pictures/profile_600.png" />
+          <meta property="og:description" content={description} />
         </Head>
         <section className="content__section">
           <div className="content__wrapper grid-content-wrapper">
             <div className="grid-column">
               <h2>Welcome</h2>
               <p>
-                Welcome to my blog, here you may find interesting content about web app development.
+                Welcome to my blog, I'll be posting about web app development, native apps, DevOps and more.
                 So if you are a developer or you just happened to visit this website randomly and want to bring your web experience to the next level, stay tuned ;)
               </p>
               </div>
