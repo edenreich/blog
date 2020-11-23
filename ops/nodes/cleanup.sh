d9875f756ebdc836d977f0b2542c45536eccb576
Eden Reich <eden.reich@gmail.com>
2020-11-23 23:08:58 +0100

    of course :)

--- ops/nodes/cleanup.sh
+++ ops/nodes/cleanup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-term=$1
-
-if [ -z $1 ]
-then
-    echo "Missing first argument, image name!"
-    exit 1
-fi
-
 echo "==> Removing all unused images on $(hostname)."
 
 sudo k3s crictl rmi --prune || true
