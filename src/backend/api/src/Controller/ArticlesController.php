1ce75ebe26cca53543ad095792c3990d2a5cb1a4
Eden Reich <eden.reich@gmail.com>
2020-12-18 01:49:24 +0100

    add tests

--- src/backend/api/src/Controller/ArticlesController.php
+++ src/backend/api/src/Controller/ArticlesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,10 +15,28 @@ class ArticlesController extends AbstractController
     /**
      * @Route("/articles", name="articles")
      */
-    public function index(SerializerInterface $serializer, EntityManagerInterface $entityManager): Response
+    public function list(SerializerInterface $serializer, EntityManagerInterface $entityManager): Response
     {
         $articles = $entityManager->getRepository(Article::class)->findAll();
 
         return new Response($serializer->serialize($articles, 'json', ['groups' => ['admin', 'frontend']]));
+    }
+
+    /**
+     * @Route("/articles/{id}", name="article")
+     */
+    public function find(string $id, SerializerInterface $serializer, EntityManagerInterface $entityManager): Response
+    {
+        try {
+            $article = $entityManager->getRepository(Article::class)->findOneBy(['id' => $id]);
+
+            if (!$article) {
+                throw new \Exception(sprintf('could not find article with id %s', $id));
+            }
+        } catch (\Exception $exception) {
+            return new JsonResponse(['message' => sprintf('could not find article with id %s', $id)], 404);
+        }
+
+        return new Response($serializer->serialize($article, 'json', ['groups' => ['admin', 'frontend']]));
     }
 }
--- src/backend/api/tests/ArticleTest.php
+++ src/backend/api/tests/ArticleTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
-class ArticleTest extends TestCase
+class ArticleTest extends KernelTestCase
 {
     public function testCanFetchAllArticles()
     {
@@ -15,5 +18,44 @@ class ArticleTest extends TestCase
 
         $articles = json_decode($response->getBody());
         $this->assertCount(10, $articles);
+    }
+
+    public function testCanFetchSingleArticle()
+    {
+        $client = new Client(['base_uri' => 'http://127.0.0.1']);
+        
+        /** @var EntityManager */
+        $em = self::bootKernel()
+            ->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        /** @var Article */
+        $article = $em->getRepository(Article::class)->findAll()[0];
+        $title = $article->getTitle();
+
+        $response = $client->get(sprintf('/articles/%s', $article->getId()));
+
+        $article = json_decode($response->getBody());
+        $this->assertEquals($title, $article->title);
+    }
+
+    public function testGetting404EmptyResponseWhenNoArticleFound()
+    {
+        $client = new Client(['base_uri' => 'http://127.0.0.1']);
+        
+        /** @var EntityManager */
+        $em = self::bootKernel()
+            ->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        try {
+            $response = $client->get('/articles/nonexistingarticle');
+            $this->assertNotEquals(200, $response->getStatusCode());
+        } catch (ClientException $exception) {
+            $this->assertEquals(404, $exception->getResponse()->getStatusCode());
+            $this->assertEquals('could not find article with id nonexistingarticle', json_decode($exception->getResponse()->getBody())->message);
+        }
     }
 }
