e2d887ea046d6210b247826ca68445ded630bb37
Eden Reich <eden.reich@gmail.com>
2020-12-22 22:46:08 +0100

    apply some path fixes and add actions buttons to datatables
    also upgrade datatables to latest version

--- src/backend/admin/src/Controller/ContentController.php
+++ src/backend/admin/src/Controller/ContentController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ContentController extends CategoriesAwareController
 {
@@ -21,12 +23,26 @@ class ContentController extends CategoriesAwareController
      */
     public function list(): Response
     {
-        // @todo send an api request to fetch all articles.
-        $articles = [];
+        if ($this->request->isXmlHttpRequest()) {
+            $client = new Client(['base_uri' => 'https://admin.eden-reich.com']);
+            $response = $client->get('/articles');
+            $articles = json_decode($response->getBody(), true);
+            $articles = array_map(function($article) {
+                return [
+                    'id' => (int) $article['id'],
+                    'title' => $article['title'],
+                    'slug' => $article['slug'],
+                    'meta_keywords' => $article['meta_keywords'],
+                    'meta_description' => $article['meta_description'],
+                    'published' => $article['published'],
+                    'created_at' => $article['created_at'],
+                    'updated_at' => $article['updated_at'],
+                ];
+            }, $articles);
+            return new JsonResponse($articles);
+        }
 
-        return $this->render('content/list.html.twig', [
-            'articles' => $articles,
-        ]);
+        return $this->render('content/list.html.twig');
     }
 
     /**
