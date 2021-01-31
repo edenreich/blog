15f4de92ea254250a3e93be4f92c53a750fd150b
Eden Reich <eden.reich@gmail.com>
2021-01-31 20:04:53 +0100

    make bucket publicly accessible

--- ops/cloud/terraform/google_storage_bucket.tf
+++ ops/cloud/terraform/google_storage_bucket.tf
@@ -1,9 +1,15 @@
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = "eden-reich-com-assets"
+  role   = "READER"
+  entity = "allUsers"
+}
+
 resource "google_storage_bucket" "assets" {
-  name          = "eden-reich.com"
+  name          = "eden-reich-com-assets"
   location      = "europe-west1"
   force_destroy = true
 
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
 
   cors {
     origin          = ["*"]
