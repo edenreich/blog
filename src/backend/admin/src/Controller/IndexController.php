61949b0271f86af526f0425a4a2c4e533925695a
Eden Reich <eden.reich@gmail.com>
2021-02-07 15:49:00 +0100

    force redirect to dashboard

---
+++ src/backend/admin/src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends NavigationAwareController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="index")
+     */
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('navigation_parent_dashboard');
+    }
+}
