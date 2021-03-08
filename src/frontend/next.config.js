6d81fc3a8982fc55b8ed934a22a902f03131ff0d
Eden Reich <eden.reich@gmail.com>
2021-03-08 16:18:33 +0100

    @zeit plugins are no longer needed, they are built-in

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,10 +1,8 @@
-const withSass = require('@zeit/next-sass');
-const withCss = require('@zeit/next-css');
 const path = require('path');
 const fs = require('fs');
 const env = process.env.APP_ENV || 'production'; 
 
-module.exports = withSass(withCss({
+module.exports = {
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname);
 
@@ -25,4 +23,4 @@ module.exports = withSass(withCss({
     app: JSON.parse(fs.readFileSync(path.join('config', 'environments', env, 'app.json'), { encoding: 'utf-8' })),
     apis: JSON.parse(fs.readFileSync(path.join('config', 'environments', env, 'apis.json'), { encoding: 'utf-8' }))
   }
-}));
+};
