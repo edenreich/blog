06faf36bff258a487d163e3ee9a1ac8e9c0b1cc6
Eden <eden.reich@gmail.com>
2021-05-22 20:28:40 +0200

    add koa bodyparser

--- authentication/src/server.ts
+++ authentication/src/server.ts
@@ -1,13 +1,16 @@
 import Koa from 'koa';
 import Logger from 'koa-logger';
+import BodyParser from 'koa-bodyparser';
 import router from './routes/api';
 import config from './config';
 
 const app: Koa = new Koa();
 
 app.use(Logger());
+app.use(BodyParser());
 
 app.use(router.routes());
+app.use(router.allowedMethods());
 app.listen(config.port, () => {
   for (const route of router.stack) {
     console.table([{ Route: route.name, Methods: route.methods, Path: route.path }]);
