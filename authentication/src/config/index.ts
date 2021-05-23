3fc17dd459df70217b7d4e518119398a18a41139
Eden <eden.reich@gmail.com>
2021-05-24 00:12:48 +0200

    connect explicitly with version and utf8

--- authentication/src/config/index.ts
+++ authentication/src/config/index.ts
@@ -2,5 +2,5 @@ export default {
   port: process.env.PORT || 3000,
   app_env: process.env.APP_ENV || 'development',
   app_secret: process.env.APP_SECRET || 'secret',
-  database_url: process.env.DATABASE_URL || 'postgres://postgres:secret@127.0.0.1:5432/blog_authentication',
+  database_url: process.env.DATABASE_URL || 'postgres://postgres:secret@127.0.0.1:5432/blog_authentication?serverVersion=13&charset=utf8',
 } as const;
