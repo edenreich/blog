8609eaa315d6392dd81b7aa8fd741f435e896600
Eden <eden.reich@gmail.com>
2021-06-19 18:59:21 +0200

    add user type definitions

---
+++ admin/src/entities/user.ts
@@ -0,0 +1,22 @@
+
+export interface IRole {
+  id: string;
+  name: 'USER_ROLE' | 'ADMIN_ROLE';
+}
+
+export interface IUser {
+  id: string;
+  username: string;
+  roles: IRole[]; 
+}
+
+export interface IAuthenticateableDTO {
+  username: string;
+  password: string;
+}
+
+export interface IAuthenticatedUser {
+  data: IUser;
+  iat: number;
+  exp: number;
+}
