3874813d9b07ed28f79233395e7fabadf1cb4958
Eden <eden.reich@gmail.com>
2021-06-19 15:11:09 +0200

    use ejs templates instead of twig

--- admin/src/routes/web.ts
+++ admin/src/routes/web.ts
@@ -1,17 +1,10 @@
 import config from '@app/config';
 import { auth } from '@app/middlewares/auth';
 import axios, { AxiosResponse } from 'axios';
-import { Context } from 'koa';
+import { DefaultContext } from 'koa';
 import Router from 'koa-router';
-import { resolve } from 'path';
 
 const router: Router = new Router();
-
-type EntryPoint = { css: string[], js: string[] };
-
-interface IEntryPointManifest {
-  entrypoints: { [key: string]: EntryPoint };
-}
 
 router.get('web.healthcheck', '/healthcheck', (ctx: any) => {
   ctx.body = {
@@ -20,22 +13,14 @@ router.get('web.healthcheck', '/healthcheck', (ctx: any) => {
   ctx.status = 200;
 });
 
-router.get('web.login', '/login', async (ctx: Context | any) => {
-  const manifest: IEntryPointManifest = await import(resolve(__dirname, '../static/entrypoints.json'));
-
-  // todo - find a way to implement this function for all views
-  // maybe also use standard webpack instead of encore
-  await ctx.render('security/login.html.twig', {
-    encore_entry_link_tags: (entry: string) => {
-      return manifest.entrypoints[entry]?.css.map((path: string) => `<link href="${path}" rel="stylesheet">`).join('\n');
-    },
-    encore_entry_script_tags: (entry: string) => {
-      return manifest.entrypoints[entry]?.js.map((path: string) => `<script src="${path}"></script>`).join('\n');
-    },
+router.get('web.login', '/login', async (ctx: DefaultContext) => {
+  await ctx.render('security/login', {
+    layout: 'auth.layout',
+    title: 'Login',
   });
 });
 
-router.post('web.login', '/login', async (ctx: Context | any) => {
+router.post('web.login', '/login', async (ctx: DefaultContext) => {
   try {
     const response: AxiosResponse = await axios.post(`${config.authentication_url}/api/authentication/jwt`, {
       username: ctx.request.body.username,
@@ -52,22 +37,13 @@ router.post('web.login', '/login', async (ctx: Context | any) => {
   }
 });
 
-router.get('web.dashboard', '/dashboard', auth, async (ctx: Context | any) => {
-  const navigation: [] = [];
-  const manifest: IEntryPointManifest = await import(resolve(__dirname, '../static/entrypoints.json'));
+router.get('web.dashboard', '/dashboard', auth, async (ctx: DefaultContext) => {
   await ctx.render('dashboard/index.html.twig', {
     user: ctx.state.user.data,
-    navigation,
-    encore_entry_link_tags: (entry: string) => {
-      return manifest.entrypoints[entry]?.css.map((path: string) => `<link href="${path}" rel="stylesheet">`).join('\n');
-    },
-    encore_entry_script_tags: (entry: string) => {
-      return manifest.entrypoints[entry]?.js.map((path: string) => `<script src="${path}"></script>`).join('\n');
-    },
   });
 });
 
-router.get('web.logout', '/logout', auth, async (ctx: Context | any) => {
+router.get('web.logout', '/logout', auth, async (ctx: DefaultContext) => {
   ctx.set('Set-Cookie', `token=deleted;expires=${new Date(new Date().setHours(new Date().getHours() - 1)).toUTCString()}`);
   ctx.response.redirect('/login');
 });
--- admin/src/server.ts
+++ admin/src/server.ts
@@ -3,34 +3,20 @@ import Koa from 'koa';
 import BodyParser from 'koa-bodyparser';
 import Logger from 'koa-logger';
 import KoaStatic from 'koa-static';
-import KoaViews from 'koa-views';
-import { resolve } from 'path';
-import { readFileSync } from 'fs';
+import ejs from 'koa-ejs';
+import { join } from 'path';
 
 export const server: Koa = new Koa();
 
-// todo - check why this global functions doesn't work, if it's not possible switch to a different template engine
-// type EntryPoint = { css: string[], js: string[] };
-// interface IEntryPointManifest {
-//   entrypoints: { [key: string]: EntryPoint };
-// }
-// const manifest: IEntryPointManifest = JSON.parse(readFileSync(resolve(__dirname, './static/entrypoints.json'), {encoding: 'utf-8'}));
-server.use(KoaStatic(resolve(__dirname, 'static')));
-server.use(KoaViews(resolve(__dirname, 'templates'), {
-  options: {
-    namespaces: { 
-      admin: resolve(__dirname, 'templates') 
-    },
-    // functions: {
-    //   encore_entry_link_tags: (entry: string): string => {
-    //     return manifest.entrypoints[entry]?.css.map((path: string) => `<link href="${path}" rel="stylesheet">`).join('\n');
-    //   },
-    //   encore_entry_script_tags: (entry: string): string => {
-    //     return manifest.entrypoints[entry]?.js.map((path: string) => `<script src="${path}"></script>`).join('\n');
-    //   },
-    // }
-  },
-}));
+ejs(server, {
+  root: join(__dirname, 'templates'),
+  layout: 'admin.layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false,
+});
+
+server.use(KoaStatic(join(__dirname, 'static')));
 server.use(Logger());
 server.use(BodyParser());
 server.use(router.routes());
