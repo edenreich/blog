b0e69e4a05af55ecaf78389c87edc81b3da921f0
Eden <eden.reich@gmail.com>
2021-07-11 19:32:37 +0200

    use the same convention as in the docs

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,28 +2,29 @@ import Fastify, { FastifyInstance } from 'fastify';
 import swagger from 'fastify-swagger';
 import routes from './routes/api';
 
-const app: FastifyInstance = Fastify({
+const fastify: FastifyInstance = Fastify({
   logger: true,
 });
 
-app.addHook('onClose', async (instance, done) => {
-  app.close();
+fastify.addHook('onClose', async (instance, done) => {
+  fastify.close();
   done();
 });
-app.register(swagger, {
+fastify.register(swagger, {
   exposeRoute: true,
   routePrefix: '/docs',
   swagger: {
     info: { version: 'v1', title: 'API' },
   },
 });
-app.register(routes);
+fastify.register(routes);
+
 
 const start = async (): Promise<void> => {
   try {
-    await app.listen(process.env.PORT || 3000, '0.0.0.0');
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
   } catch (error) {
-    app.log.error(error);
+    fastify.log.error(error);
     process.exit(1);
   }
 };
